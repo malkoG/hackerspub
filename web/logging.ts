2d346744156c7368991f237578f7c0ea266ae7d9
Hong Minhee <hong@minhee.org>
2025-05-03 15:54:26 +0900

    `LOG_QUERY` option

--- web/logging.ts
+++ web/logging.ts
@@ -1,6 +1,8 @@
 import { ansiColorFormatter, configure, getStreamSink } from "@logtape/logtape";
 import { AsyncLocalStorage } from "node:async_hooks";
 
+const LOG_QUERY = Deno.env.get("LOG_QUERY")?.toLowerCase() === "true";
+
 await configure({
   contextLocalStorage: new AsyncLocalStorage(),
   sinks: {
@@ -14,7 +16,11 @@ await configure({
       lowestLevel: "debug",
       sinks: ["console"],
     },
-    { category: "drizzle-orm", lowestLevel: "info", sinks: ["console"] },
+    {
+      category: "drizzle-orm",
+      lowestLevel: LOG_QUERY ? "debug" : "info",
+      sinks: ["console"],
+    },
     { category: "fedify", lowestLevel: "info", sinks: ["console"] },
     {
       category: ["logtape", "meta"],
